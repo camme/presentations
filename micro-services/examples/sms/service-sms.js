1bdcbfa107c8b8f83e8f1f1c0c31a2b469857242
Camilo Tapia <camilo.tapia@24hr.se>
2014-10-13 14:46:04 +0200

    added sms example

---
+++ micro-services/examples/sms/service-sms.js
@@ -0,0 +1,50 @@
+var zmq = require('zmq');
+var zonar = require('zonar');
+var elks = require('sms-46elks');
+
+// configs
+var port = 6010;
+var address = "tcp://0.0.0.0:" + port;
+
+// zmq socket
+var socket = zmq.socket('pub');
+
+// sms endpoint
+var smsServer = elks.create('/46elks/sms', 8787);
+
+// zonar setup
+var broadcaster = zonar.create({ net: 'foocafe', name: 'sms', payload: { port: port } });
+
+// begin
+smsServer.init(function() {
+
+    socket.bind(address, function(err) {
+
+        if (err) throw err;
+
+        console.log("SMS publishing service started");
+
+        broadcaster.start(function() {
+            console.log("Broadcasting SMS service");
+        });
+
+        smsServer.on('incoming', function(sms) {
+            console.log('SMS Service got "%s" from %s', sms.message, sms.from);
+            socket.send('all ' + JSON.stringify(sms));
+        });
+
+    });
+
+});
+
+
+// greacefully quit
+process.on('SIGINT', function() {
+    console.log("");
+    broadcaster.stop(function() {
+        console.log("Zonar has stoped");
+        socket.close(function() { });
+        process.exit(0);
+    });
+});
+
